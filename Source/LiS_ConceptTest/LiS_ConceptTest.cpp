d41112ba807ec96ae00106759645fe70869e413e
Louis Stockdale-Lees <lslees1346@gmail.com>
2024-05-21 13:57:16 +0100

    Game

---
+++ Source/LiS_ConceptTest/LiS_ConceptTest.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LiS_ConceptTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LiS_ConceptTest, "LiS_ConceptTest" );
